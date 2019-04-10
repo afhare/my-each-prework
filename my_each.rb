06886beefa28f118d592fa38b90c5fdbc0dab697
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-10 12:39:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array) # put argument(s) here
+  counter = 0 
+  while counter < array.length do
+    
+    counter +=1
+    end# code here
 end
